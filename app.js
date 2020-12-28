0ce39b00c3d77337f9aeca32f85f1c0142240955
diego <diego.atayra@gmail.com>
2020-12-28 01:43:03 -0300

    using ES6 classes

--- app.js
+++ app.js
@@ -1,88 +1,139 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 canvas.height = 630;
 canvas.width = 1100;
-paused = true;
 
+const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
-const res = 6;
-const cols = Math.floor(canvas.width / res);
-const rows = Math.floor(canvas.height / res);
 
-const build = () =>
+class Game
 {
-    let tmp = Array(cols);
-    for (let i = 0; i < cols; i++)
-        tmp[i] = Array(rows);
-    return tmp;
+    constructor(cols, rows)
+    {
+        this.cols = cols;
+        this.rows = rows;
+        this.paused = true;
+        this.grid = this.build();
+    }
+
+    build()
+    {
+        let tmp = Array(this.cols);
+        for (let i = 0; i < this.cols; i++)
+            tmp[i] = Array(this.rows);
+        return tmp;
+    }
+    
+    forEachCell(call)
+    {
+        for (let i = 0; i < this.cols; i++)
+            for (let j = 0; j < this.rows; j++)
+                call(i, j, this.grid[i][j]);
+    }
+
+    randomize()
+    {
+        this.forEachCell((x,y) => this.grid[x][y] = Math.floor(Math.random()*50) < 3 ? 1 : 0);                
+    }
+
+    swap(x, y)
+    {
+        this.grid[x][y] = (this.grid[x][y] + 1) % 2;
+    }
+
+    tooglePause()
+    {
+        this.paused = !this.paused;
+    }
+
+    step()
+    {
+        if (this.paused)
+            return;
+
+        let next = this.build();
+        this.forEachCell((i,j) => {
+                let n = this.neighboursOf(i,j);
+                if (n === 3)
+                    next[i][j] = 1;
+                else if (n < 2 || n > 3)
+                    next[i][j] = 0;
+                else
+                    next[i][j] = this.grid[i][j];
+        });
+        this.grid = next;    
+    }
+
+    neighboursOf(x, y)
+    {
+        let sum = 0;
+        for(let i = -1; i < 2; i++)
+            for(let j = -1; j < 2; j++)
+                sum += this.grid[(x+i+this.cols) % this.cols][(y+j+this.rows) % this.rows];
+
+        sum -= this.grid[x][y];
+        return sum;
+    }
 }
 
-grid = build();
-
-for (let i = 0; i < cols; i++)
-    for (let j = 0; j < rows; j++)
-        grid[i][j] = Math.floor(Math.random()*50) < 3 ? 1 : 0;
-
-const draw = () =>
+class App
 {
-    for (let i = 0; i < cols; i++)
-        for (let j = 0; j < rows; j++)
-        {
-            if (grid[i][j] === 1)
-                ctx.fillRect(i*res, j*res, res, res);
-        }
+    constructor(canvas, resolution)
+    {
+        this.cols = Math.floor(canvas.width / resolution);
+        this.rows = Math.floor(canvas.height / resolution);
+        this.resolution = resolution;
+        this.game = new Game(this.cols, this.rows);
+        this.initialize(canvas);
+    }
+
+    initialize(canvas)
+    {
+        window.addEventListener("keypress", this.keyPressed);
+        canvas.addEventListener("click", this.click);
+    }
+
+    keyPressed(e)
+    {
+        if (e.key === 'Enter')
+            app.game.tooglePause();
+    }
+
+    click(e)
+    {
+        const x = Math.floor((e.clientX - e.target.offsetLeft) / app.resolution);
+        const y = Math.floor((e.clientY - e.target.offsetTop) / app.resolution);
+        app.game.swap(x,y);
+    }
+
+    ramdomize()
+    {
+        this.game.randomize();
+    }
+
+    draw()
+    {
+        this.game.forEachCell((i,j,s) => {
+                if (s === 1)
+                    this.paint(i, j, this.resolution);
+        });
+    }
+
+    step()
+    {
+        this.game.step();
+    }
 }
 
-neighboursOf = function(x, y)
-{
-    let sum = 0;
-    for(let i = -1; i < 2; i++)
-        for(let j = -1; j < 2; j++)
-            sum += grid[(x+i+cols) % cols][(y+j+rows) % rows];
+app = new App(canvas, 5);
+app.ramdomize();
+app.paint = (x, y, size) => ctx.fillRect(x * size, y * size, size, size);
 
-    sum -= grid[x][y];
-    return sum;
-}
-
-const step = () =>
-{
-    let next = build();
-    for(let i = 0; i < cols; i++)
-        for(let j = 0; j < rows; j++)
-        {
-            n = neighboursOf(i,j);
-            if (n === 3)
-                next[i][j] = 1;
-            else if (n < 2 || n > 3)
-                next[i][j] = 0;
-            else
-                next[i][j] = grid[i][j];
-        }
-    grid = next;
-}
-
-const keyPressed = (e) =>
-{
-    if (e.key === 'Enter')
-        paused = !paused;
-}
-
-const click = (e) =>
-{
-    const x = Math.floor((e.clientX-canvas.offsetLeft) / res);
-    const y = Math.floor((e.clientY-canvas.offsetTop) / res);
-    grid[x][y] = (grid[x][y] + 1) % 2;
-}
-
-window.addEventListener("keypress", keyPressed);
-canvas.addEventListener("click", click);
 
 const loop = () =>
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw();
-    if (!paused)
-        step();
+    app.draw();
+    app.step();
     window.requestAnimationFrame(loop);
 }
 window.requestAnimationFrame(loop);
